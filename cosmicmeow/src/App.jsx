099f2956d92e10cd9aaf3eb964ea6318b056f050
Lookmai Rattana <oratt001@gmail.com>
2019-09-25 20:18:44 -0400

    added landing and services section

--- cosmicmeow/src/App.jsx
+++ cosmicmeow/src/App.jsx
@@ -2,29 +2,53 @@ import React from 'react';
 import './App.scss';
 
 class App extends React.Component {
+	LandingSection = () => {
+		return (
+			<div id='landing' class='section-container hero-banner'>
+				<div
+					id='landing-back'
+					class='landing-container parallax-container'
+				></div>
+				<div
+					id='landing-middle'
+					class='landing-container parallax-container'
+				></div>
+				<div id='landing-front' class='landing-container'>
+					<div class='clouds'></div>
+					<div class='hello'></div>
+					<div class='bun'></div>
+				</div>
+			</div>
+		);
+  };
 
-  LandingSection = () => {
-    return (<div id="landing" class="section-container hero-banner">
-    <div id="landing-back" class="landing-container parallax-container">
-      </div>
-    <div id="landing-middle" class="landing-container parallax-container">
-      </div>
-    <div id="landing-front" class="landing-container">
-      <div class="clouds"></div>
-      <div class="hello"></div>
-      <div class="bun"></div>
-  </div>
-    
-  </div>)
-  }
-  render() {
+  ServicesSection = () => {
     return (
-      <div className="app-root">
-        <this.LandingSection/>
-      </div>
+      <div class="section-container services">
+      <ul class="services-items"> 
+        <li id="services-web" class="services-item">
+          <div class="services-image"></div>
+          <div class="services-copy">I code!</div>
+          <div class="services-subcopy">I currently code full time.<br/>Not available for freelance coding job atm.</div>
+        </li>
+        <li id="services-illustration" class="services-item">
+          <div class="services-image"></div>
+          <div class="services-copy">I also draw!</div>
+          <div class="services-subcopy">Commission available!<br/><a href="mailto:cosmicmeow.works@gmail.com" target="_blank">Get in touch!</a></div>
+        </li>
+      </ul>
+    </div>
     );
   }
   
+	render() {
+		return (
+			<div className='app-root'>
+				<this.LandingSection />
+        <this.ServicesSection/>
+			</div>
+		);
+	}
 }
 
 export default App;
