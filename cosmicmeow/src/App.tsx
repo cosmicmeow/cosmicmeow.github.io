0ca54bda71be505ffc60d9b428d636753f711564
Lookmai Rattana <oratt001@gmail.com>
2019-10-09 00:57:19 -0400

    finished up stickers and about me sections

--- cosmicmeow/src/App.tsx
+++ cosmicmeow/src/App.tsx
@@ -1,22 +1,8 @@
 import React from 'react';
 import './App.scss';
 import WebWorks from './WebWorks';
-
-interface WorkItemType {
-	key: string;
-	image: string;
-	title: string;
-	subtitle: string;
-	clients: string;
-	tools: string;
-	url: string;
-}
-
-interface HeaderItemType {
-	icon: string;
-	title: string;
-	subtitle: string;
-}
+import Illustration from './Illustrations';
+import SectionHeader from './SectionHeader';
 
 class App extends React.Component {
 	LandingSection = () => {
@@ -74,9 +60,69 @@ class App extends React.Component {
 				<this.LandingSection />
 				<this.ServicesSection />
 				<WebWorks />
+				<Illustration />
+				<this.AboutMe />
 			</div>
 		);
 	}
+
+	AboutMe = () => {
+		const item = {
+			icon: 'bun_about.png',
+			title: 'About Lookmai',
+			subtitle:
+				'Lookmai is a freelance frontend developer and illustrator. She finished CS bachelor at Old Dominion University and is currently working at Anthem.',
+		};
+
+		const emailurl =
+			'mailto:cosmicmeow.works@gmail.com?&subject=Hello%20There!&body=Your-inquiry-goes-here!';
+		return (
+			<div id='about' className='section-container'>
+				<div className='page-container'>
+					<SectionHeader item={item} />
+					<h4 style={{ maxWidth: '430px', margin: 'auto' }}>
+						I am currently available for illustration commissions
+						and full time frontend development opportunity!
+						<a
+							className='inline-email-link'
+							href={emailurl}
+							target='_blank'>
+							Get in touch!
+						</a>
+					</h4>
+					<a
+						href='https://www.linkedin.com/in/lookmai-rattana-23365966/'
+						target='_blank'>
+						<img
+							className='lookmai-image'
+							src={require('./assets/landing_assets/lookmai.png')}></img>
+					</a>
+					<div className='social-links-container'>
+						<a
+							href='https://twitter.com/cosmicmeoww'
+							target='_blank'>
+							<img
+								className='social-link'
+								src={require('./assets/landing_assets/icon_twitter.png')}></img>
+						</a>
+						<a
+							href='https://instagram.com/cosmicmeowdraws'
+							target='_blank'>
+							<img
+								className='social-link'
+								src={require('./assets/landing_assets/icon_instagram.png')}></img>
+						</a>
+
+						<a href={emailurl} target='_blank'>
+							<img
+								className='social-link'
+								src={require('./assets/landing_assets/icon_email.png')}></img>
+						</a>
+					</div>
+				</div>
+			</div>
+		);
+	};
 }
 
 export default App;
